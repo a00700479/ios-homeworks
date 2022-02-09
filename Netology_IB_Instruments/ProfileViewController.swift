84f0edae29dc9a60c210400258e9130c4ff4a99a
a00700479 <a00700479@gmail.com>
2022-02-10 00:57:03 +0300

    Добавила экран ProfileView (включает фото, лейблы с информацией и текст) согласно заданию 1.

---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Natali on 09.02.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var profileView: ProfileView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    }
+
